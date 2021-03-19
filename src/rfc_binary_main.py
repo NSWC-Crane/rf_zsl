be473e702e66b7eac03ed755de2a3c971419b031
javier <javic9788@gmail.com>
2021-03-19 13:47:25 -0400

    Update rfc_binary_main.py

--- src/rfc_binary_main.py
+++ src/rfc_binary_main.py
@@ -10,33 +10,18 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 import datetime
 
-'''
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.encoder_hidden_layer = nn.Linear(
-            in_features=kwargs["input_shape"], out_features=128
-        )
-        self.encoder_output_layer = nn.Linear(
-            in_features=128, out_features=128
-        )
-        self.decoder_hidden_layer = nn.Linear(
-            in_features=128, out_features=128
-        )
-        self.decoder_output_layer = nn.Linear(
-            in_features=128, out_features=kwargs["input_shape"]
-        )
-'''
 
 ###torch.manual_seed(42)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-max_epochs = 24000
+max_epochs = 300000
 
 # number of random samples to generate (should be a multiple of two for flattening an IQ pair)
-input_size = 256
-feature_size = 1
-decoder_int1 = 64
+input_size = 8
+feature_size = 3
+decoder_int1 = 128
+m = 36
 
 read_data = True
 
@@ -79,9 +64,11 @@ class Decoder(nn.Module):
     def __init__(self, output_size, feature_size):
         super().__init__()
         self.input_layer = nn.Linear(feature_size, decoder_int1, bias=False)
-        self.hidden_layer_1 = nn.Linear(decoder_int1, 128, bias=False)
-        self.hidden_layer_2 = nn.Linear(64, decoder_int1, bias=False)
-        self.output_layer = nn.Linear(128, output_size, bias=False)
+        self.hidden_layer_1 = nn.Linear(decoder_int1, 256, bias=False)
+        self.hidden_layer_2 = nn.Linear(256, 5412, bias=False)
+        self.hidden_layer_3 = nn.Linear(512, 1024, bias=False)
+        self.hidden_layer_4 = nn.Linear(1024, 2048, bias=False)
+        self.output_layer = nn.Linear(2048, output_size, bias=False)
         #self.prelu = nn.PReLU(1, 0.25)
         # self.multp = nn.Parameter(torch.tensor([[128.0]]))
         #self.silu = nn.SiLU()
@@ -99,7 +86,9 @@ class Decoder(nn.Module):
         #activation = self.relu(activation)
         # activation = activation*self.multp.expand_as(activation)
         activation = self.hidden_layer_1(activation)
-        #activation = self.hidden_layer_2(activation)
+        activation = self.hidden_layer_2(activation)
+        activation = self.hidden_layer_3(activation)
+        activation = self.hidden_layer_4(activation)
         #activation = self.silu(activation)
         activation = self.output_layer(activation)
         #activation = self.tanh(activation)
@@ -111,21 +100,16 @@ class Decoder(nn.Module):
 
     def round_weights(self):
         with torch.no_grad():
-            t1 = self.input_layer.weight.data
-            t1 = 2*(t1 > 0).type(torch.float32) - 1
-            #t1 = torch.floor(t1*m + 0.5)/m
-            # t1 = torch.clamp_min(torch.clamp_max(self.multp*t1, 32), -32)
-            # t1 = torch.floor(t1+0.5)/self.multp
-            t1 = torch.floor(t1/128)
-            self.input_layer.weight.data = t1
-
-            t2 = self.output_layer.weight.data
-            t2 = 2*(t2 > 0).type(torch.float32) - 1
-            #t2 = torch.floor(t2*m + 0.5)/m
-            # t2 = torch.clamp_min(torch.clamp_max(self.multp*t2, 32), -32)
-            # t2 = torch.floor(t2+0.5)/self.multp
-            t2 = torch.floor(t2 / 128)
-            self.output_layer.weight.data = t2
+            for param in model.decoder.parameters():
+                # t1 = model.decoder.input_layer.weight.data
+                # t1 = 2 * (t1 > 0).type(torch.float32) - 1
+                # t1 = torch.floor(t1*m + 0.5)/m
+                # t1 = torch.clamp_min(torch.clamp_max(m*t1, 16), -16)
+                # t1 = torch.floor(t1+0.5)/m
+                t1 = param.data
+                t1 = 2 * (t1 > 0).type(torch.float32) - 1
+                t1 = t1 / m
+                param.data = t1
 
 # use the encoder and decoder classes to build the autoencoder
 class AE(nn.Module):
@@ -145,10 +129,10 @@ device = "cpu"
 model = AE(input_size, feature_size).to(device)
 
 # cycle through the decoder side and do whatever needs doing
-for parameter in model.decoder.parameters():
-    #parameter.requires_grad = False        # uncomment to freeze the training of these layers
-    p = parameter.data
-    bp = 0
+# for parameter in model.decoder.parameters():
+#     #parameter.requires_grad = False        # uncomment to freeze the training of these layers
+#     p = parameter.data
+#     bp = 0
 
 #model.decoder.prelu.weight.requires_grad = False
 
@@ -159,8 +143,11 @@ with torch.no_grad():
 
     # random values of -1/1
     model.decoder.input_layer.weight.data = nn.Parameter(torch.from_numpy(2*(mr.uniform(0, 1.0, [decoder_int1, feature_size]) > 0.5).astype(np.float32)-1)).to(device)
-    model.decoder.hidden_layer_1.weight.data = nn.Parameter(torch.from_numpy(2 * (mr.uniform(0, 1.0, [128, decoder_int1]) > 0.5).astype(np.float32) - 1)).to(device)
-    model.decoder.output_layer.weight.data = nn.Parameter(torch.from_numpy(2*(mr.uniform(0, 1.0, [input_size, 128]) > 0.5).astype(np.float32)-1)).to(device)
+    model.decoder.hidden_layer_1.weight.data = nn.Parameter(torch.from_numpy(2 * (mr.uniform(0, 1.0, [256, decoder_int1]) > 0.5).astype(np.float32) - 1)).to(device)
+    model.decoder.hidden_layer_2.weight.data = nn.Parameter(torch.from_numpy(2 * (mr.uniform(0, 1.0, [512, 256]) > 0.5).astype(np.float32) - 1)).to(device)
+    model.decoder.hidden_layer_3.weight.data = nn.Parameter(torch.from_numpy(2 * (mr.uniform(0, 1.0, [1024, 512]) > 0.5).astype(np.float32) - 1)).to(device)
+    model.decoder.hidden_layer_4.weight.data = nn.Parameter(torch.from_numpy(2 * (mr.uniform(0, 1.0, [2048, 1024]) > 0.5).astype(np.float32) - 1)).to(device)
+    model.decoder.output_layer.weight.data = nn.Parameter(torch.from_numpy(2*(mr.uniform(0, 1.0, [input_size, 2048]) > 0.5).astype(np.float32)-1)).to(device)
 
     # make a deep copy of the weights to make sure they don't change
     ew1 = copy.deepcopy(model.encoder.input_layer.weight)
@@ -171,22 +158,7 @@ with torch.no_grad():
 # this is setup as a static learning rate.  we may want to look at variable lr based on some performance numbers
 #optimizer = optim.Adam(model.parameters(), lr=1e-3)
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=5e-3)
-#scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-1, div_factor=100, steps_per_epoch=1, epochs=max_epochs, verbose=True)
-#scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5000, threshold=0.1, threshold_mode='rel', cooldown=20, min_lr=1e-10, eps=1e-08, verbose=True)
 criterion = nn.MSELoss()
-#criterion = nn.L1Loss()
-
-'''
-transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-
-train_dataset = torchvision.datasets.MNIST(
-    root="D:/data", train=True, transform=transform, download=True
-)
-
-train_loader = torch.utils.data.DataLoader(
-    train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True
-)
-'''
 
 #cv2.setNumThreads(0)
 
@@ -206,24 +178,27 @@ if __name__ == '__main__':
     X = torch.from_numpy(x).to(device)
     X = X.view(-1, input_size)
 
+    Xc = np.copy(X)
+    Xc = torch.from_numpy(Xc).to(device)
+    Xc = Xc.view(-1, input_size)
+    X /= 2000
+
     # model must be set to train mode for QAT logic to work
     model.train()
 
-    m = 128
     lr_shift = 1.0
-    update_weights = 500
+    update_weights = 100
 
     tag = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     # Writer will output to ./runs/ directory by default
     writer = SummaryWriter(f'../runs/{tag}')
-    # writer.add_text('Binary Weights', '-1/1 weights updated every 100 epochs')
 
     for epoch in range(max_epochs):
         model.train()
         loss = 0
         optimizer.zero_grad()
         outputs = model(X)
-        #outputs = torch.floor(outputs + 0.5)
+        # outputs = torch.floor(outputs + 0.5)
 
         train_loss = criterion(outputs, X)
         train_loss.backward()
@@ -232,20 +207,8 @@ if __name__ == '__main__':
 
         if (epoch % update_weights) == 0:
             model.decoder.round_weights()
-            # with torch.no_grad():
-            #     t1 = model.decoder.input_layer.weight.data
-            #     t1 = 2*(t1 > 0).type(torch.float32) - 1
-            #     #t1 = torch.floor(t1*m + 0.5)/m
-            #     t1 = torch.clamp_min(torch.clamp_max(m*t1, 32), -32)
-            #     t1 = torch.floor(t1+0.5)/m
-            #     model.decoder.input_layer.weight.data = t1
-            #
-            #     t2 = model.decoder.output_layer.weight.data
-            #     t2 = 2*(t2 > 0).type(torch.float32) - 1
-            #     #t2 = torch.floor(t2*m + 0.5)/m
-            #     t2 = torch.clamp_min(torch.clamp_max(m*t2, 32), -32)
-            #     t2 = torch.floor(t2+0.5)/m
-            #     model.decoder.output_layer.weight.data = t2
+            outputs = model(X)
+            loss = criterion(2000*outputs, Xc)
 
         writer.add_scalar("Loss/train", loss, epoch)
         print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, max_epochs, (loss)))
@@ -262,8 +225,6 @@ if __name__ == '__main__':
         #quantized_model = torch.quantization.convert(model.eval(), inplace=False)
         #quantized_model.eval()
 
-
-
         #scheduler.step(math.floor(loss))
         #scheduler.step()
 
@@ -275,8 +236,8 @@ if __name__ == '__main__':
         loss = torch.sum(torch.abs(torch.floor(outputs + 0.5) - X))
         print("\nloss = {:.6f}".format(loss.item()))
         outputs = model(X)
-        loss = torch.sum(torch.abs(torch.floor(outputs + 0.5) - X))
-        print("\nloss3 = {:.6f}".format(loss.item()))
+        # loss = torch.sum(torch.abs(torch.floor((outputs*1000) + 0.5) - Xc))
+        # print("\nloss3 = {:.6f}".format(loss.item()))
 
         ew2 = copy.deepcopy(model.encoder.input_layer.weight)
         dw1b = copy.deepcopy(model.decoder.input_layer.weight)
@@ -302,7 +263,7 @@ if __name__ == '__main__':
 
     torch.save(model, f'../runs/{tag}/saved_model.pth')
     writer.add_hparams({'epochs': max_epochs, 'update_weights': update_weights, 'lr_shift': lr_shift,
-                        'input_size': input_size, 'feature size': feature_size, 'decoder_init1': decoder_int1},
+                        'input_size': input_size, 'feature size': feature_size, 'decoder_init1': decoder_int1, 'scale': m},
                        {'hparam/loss': loss})
     writer.add_graph(model, X)
     writer.close()
