0ba1e95c153e431fd40a19cdf3c22501081b6b45
Campos <1591706280117002@mil>
2021-07-12 15:57:07 -0400

    Deleting model.py
    Code has been moved to rfc_binary_main.py

--- src/rfc_binary_main.py
+++ src/rfc_binary_main.py
@@ -10,7 +10,6 @@ import datetime
 import os
 
 from zsl_error_metric import *
-from model import AE
 from utils import *
 
 # number of random samples to generate (should be a multiple of two for flattening an IQ pair)
@@ -29,6 +28,72 @@ init_mse_loss = 0
 best_mse_loss = 0
 
 data_file = "lfm_test_10M_100m_0000"
+
+
+# create the encoder class
+class Encoder(nn.Module):
+    def __init__(self, input_size, feature_size):
+        super().__init__()
+        self.input_layer = nn.Linear(input_size, 16, bias=False)
+        self.hidden_layer_1 = nn.Linear(16, 128, bias=False)
+        self.hidden_layer_2 = nn.Linear(128, 256, bias=False)
+        self.output_layer = nn.Linear(256, feature_size, bias=False)
+
+    def forward(self, activation):
+        activation = self.input_layer(activation)
+        activation = self.hidden_layer_1(activation)
+        activation = self.hidden_layer_2(activation)
+        activation = self.output_layer(activation)
+        return activation
+
+
+# create the decoder class
+class Decoder(nn.Module):
+    def __init__(self, output_size, feature_size):
+        super().__init__()
+        self.input_layer = nn.Linear(feature_size, output_size, bias=False)
+
+    def forward(self, activation):
+        activation = self.input_layer(activation)
+        return activation
+
+
+# use the encoder and decoder classes to build the autoencoder
+class AE(nn.Module):
+    def __init__(self, input_size, feature_size):
+        super().__init__()
+        self.encoder = Encoder(input_size, feature_size)
+        self.decoder = Decoder(input_size, feature_size)
+
+    def forward(self, features):
+        code = self.encoder(features)
+        reconstructed = self.decoder(code)
+        return reconstructed
+
+    def round_weights(self, m):
+        with torch.no_grad():
+            for param in self.decoder.parameters():
+                t1 = param.data
+                t1 = 2 * (t1 > 0).type(torch.float32) - 1
+                t1 = t1 / m
+                param.data = t1
+
+    def freeze_encoder(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def freeze_decoder(self):
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_encoder(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = True
+
+    def unfreeze_decoder(self):
+        for param in self.decoder.parameters():
+            param.requires_grad = True
+
 
 ### torch.manual_seed(42)
 torch.backends.cudnn.deterministic = True
