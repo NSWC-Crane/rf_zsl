8e14fd476b8235c9e490e2fe3be720edad7ecd90
david emerson <davemers@iu.edu>
2021-08-23 10:26:44 -0400

    Update cf_example.m

--- matlab/cf_example.m
+++ matlab/cf_example.m
@@ -27,15 +27,14 @@ iq_int = iq(:);
 %% try curve fitting
 
 iq_start = 50000;
-io_size = 36;
+io_size = 48;
 
-iq_slice = iq_int(iq_start:io_size+iq_start-1)/2048;
+iq_slice = iq_int(iq_start:io_size+iq_start-1);
 cx = (0:1:io_size-1)';
 
 [cf, cf_metrics] = fit(cx, iq_slice, 'sin3');
 % re = floor(cf(cx)+0.5);
 re = cf(cx);
-
 
 fprintf('Curve Fit:\n');
 disp(cf);
@@ -43,13 +42,27 @@ disp(cf);
 fprintf('\nFit Metrics:\n');
 disp(cf_metrics);
 
-
-plot(cf, cx, iq_slice)
-
-hold on
-plot(cx, re, 'g')
-
 % calculate the ratio (num coeff / iosize) * (coeff bits / data bits)
 ratio = (numel(coeffvalues(cf))/io_size)*(32/16);
 
 fprintf('\nRatio: %10.7f\n', ratio);
+%% plot
+figure(plot_num)
+set(gcf,'position',([50,50,1400,500]),'color','w')
+grid on
+box on
+hold on
+
+%plot the data
+scatter(cx, iq_slice, 20, 'o', 'b', 'filled')
+
+plot((0:0.01:io_size-1)', cf((0:0.01:io_size-1)'), 'r', 'LineWidth', line_width)
+
+
+
+% plot(cf, cx, iq_slice)
+
+% hold on
+scatter(cx, re, 20, '*', 'g')
+
+
