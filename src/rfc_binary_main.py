169b6b2c1943a1357b4ceeaaca9500f143df89f9
jicampos <jivcampos8@gmail.com>
2021-08-30 08:48:06 -0400

    Update rfc_binary_main.py

--- src/rfc_binary_main.py
+++ src/rfc_binary_main.py
@@ -9,13 +9,22 @@ import torch.optim as optim
 import datetime
 import os
 
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.manifold import TSNE
+from scipy import stats
+import pandas as pd
+from numpy import diff
+
 from zsl_error_metric import *
 from utils import *
 from zsl_clustering import *
 
 # number of random samples to generate (should be a multiple of two for flattening an IQ pair)
 input_size = 1024
-max_epochs = 500
+max_epochs = 1000
 feature_size = 1
 
 num_clusters_max = 50
@@ -27,6 +36,13 @@ m = 128
 final_zsl_metric = ()
 init_mse_loss = 0
 best_mse_loss = 0
+
+dist_mean_arr = []
+dist_std_arr = []
+phase_mean_arr = []
+phase_std_arr = []
+
+loss_arr = []
 
 data_file = "lfm_test_10M_100m_0000"
 
@@ -55,8 +71,8 @@ class Decoder(nn.Module):
         self.input_layer = nn.Linear(feature_size, output_size, bias=False)
 
     def forward(self, activation):
-        activation = self.input_layer(activation)
-        return activation
+        reconstructed = self.input_layer(activation)
+        return reconstructed
 
 
 # use the encoder and decoder classes to build the autoencoder
@@ -68,8 +84,12 @@ class AE(nn.Module):
 
     def forward(self, features):
         code = self.encoder(features)
+        code.to(device)
         reconstructed = self.decoder(code)
         return reconstructed
+
+    def get_encoder(self):
+        return self.encoder
 
     def round_weights(self, m):
         with torch.no_grad():
@@ -125,6 +145,7 @@ if __name__ == '__main__':
             x = rng.integers(-2048, 2048, size=(1, 1, 1, input_size), dtype=np.int16,
                              endpoint=False).astype(np.float32)
 
+
         # convert x into a torch tensor variable
         X = torch.from_numpy(x).to(device)
         X = X.view(-1, input_size)
@@ -166,6 +187,14 @@ if __name__ == '__main__':
             optimizer.step()
             loss = train_loss.item()
 
+            [dist_mean, dist_std, phase_mean, phase_std] = zsl_error_metric(X, outputs.detach().numpy())
+            dist_mean_arr.append(dist_mean)
+            dist_std_arr.append(dist_std)
+            phase_mean_arr.append(phase_mean)
+            phase_std_arr.append(phase_std)
+
+            loss_arr.append(loss)
+
             print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, max_epochs, loss))
 
             if torch.sum(torch.abs(torch.floor(outputs + 0.5) - X)) < 1:
@@ -177,8 +206,48 @@ if __name__ == '__main__':
                 lr_shift = 0.9 * lr_shift
 
         # ------------------------------------------------------------------------------
+
+        plt.figure()
+        plt.plot(loss_arr)
+        plt.title('Mean Squared Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.show()
+        # plt.savefig(f'mse_loss_nc{n_clusters}_idx{idx}')
+        plt.close()
+
+        plt.figure()
+        plt.plot(phase_mean_arr, label='phase mean')
+        plt.plot(phase_std_arr, label='phase std')
+        plt.title('Phase Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+        # plt.savefig(f'phase_loss_nc{n_clusters}_idx{idx}')
+        plt.close()
+
+        plt.figure()
+        plt.plot(dist_mean_arr, label='dist mean')
+        plt.plot(dist_std_arr, label='dist std')
+        plt.title('Dist. Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+        # plt.savefig(f'dist_loss_nc{n_clusters}_idx{idx}')
+        plt.close()
+
+        dist_mean_arr = []
+        dist_std_arr = []
+        phase_mean_arr = []
+        phase_std_arr = []
+
+        loss_arr = []
+
+        # ------------------------------------------------------------------------------
         # reassign each weight based on its nearest cluster center
-        labels, centers = cluster_weights(model, n_clusters)
+        labels, centers = cluster_weights(model, n_clusters, cluster_name='KMeans')
         optimizer = optim.Adam(model.parameters(), lr=5e-3)
         model.freeze_decoder()
 
@@ -229,23 +298,24 @@ if __name__ == '__main__':
         bp = 0
 
         # ------------------------------------------------------------------------------
+
         weights_writer = open((log_dir + scenario_name + "-" + 'TEST_weights' + "-" + date_time + ".bin"), "wb")
         model_weights = model.decoder.input_layer.weight.detach().numpy()
         weights_writer.write(model_weights.astype(np.float32))
         weights_writer.close()
 
-        read_weights = np.fromfile(log_dir + scenario_name + "-" + 'TEST_weights' + "-" + date_time + ".bin",
-                                   dtype=np.float32, count=-1, sep='', offset=0).astype(np.float32)
+        read_weights = np.fromfile(log_dir + scenario_name + "-" + 'TEST_weights' + "-" + date_time + ".bin", dtype=np.float32, count=-1, sep='', offset=0).astype(np.float32)
 
         label_writer = open((log_dir + scenario_name + "-" + 'TEST_labels' + "-" + date_time + ".bin"), "wb")
         label_writer.write(labels.astype(np.uint8))
         label_writer.close()
 
         read_labels = np.fromfile(log_dir + scenario_name + "-" + 'TEST_labels' + "-" + date_time + ".bin",
-                                  dtype=np.uint8, count=-1, sep='', offset=0).astype(np.float32)
+                                   dtype=np.uint8, count=-1, sep='', offset=0).astype(np.float32)
 
+        bp = 0
         new_model = AE(input_size, feature_size)
-        new_model.encoder = model.encoder
+        new_model.encoder = model.get_encoder()
         # new_model.decoder.input_layer.weight.data = nn.Parameter(torch.from_numpy(read_labels.reshape(model.decoder.input_layer.weight.shape)))
 
         read_labels = read_labels.reshape(-1, 1)
@@ -262,9 +332,56 @@ if __name__ == '__main__':
         read_labels = nn.Parameter(torch.from_numpy(read_labels.reshape(model.decoder.input_layer.weight.shape)))
         new_model.decoder.input_layer.weight = read_labels
 
+        bp = 0
+
         new_outputs = new_model(X)
 
         print(f'Reconstruction Loss: {criterion(new_outputs, X).item()}')
+
+        X = np.array(range(0, n_clusters)).reshape(-1, 1)
+        y = np.sort(centers, axis=0)
+        degree = 5
+
+        for degree in [3, 5, 10, 15, 40]:
+            # linear regression
+            lin_reg = LinearRegression().fit(X, y)
+            poly_reg = PolynomialFeatures(degree=degree)
+
+            # polynomial regression
+            X_poly = poly_reg.fit_transform(X)
+            poly_reg = LinearRegression().fit(X_poly, y)
+
+            plt.figure()
+            plt.scatter(X, y, color='red')
+            plt.plot(X, lin_reg.predict(X), color='blue')
+            plt.plot(X, poly_reg.predict(X_poly), color='blue')
+            plt.title(f'Linear & Polynomial Regression (degree: {degree})')
+            plt.xlabel('Label')
+            plt.ylabel('Weight')
+            plt.show()
+            # plt.savefig(f'linear_poly_regression_ordered_{degree}_nc{n_clusters}_idx{idx}')
+            plt.close()
+
+            _, count = count_freq(labels, max=n_clusters)
+
+            plt.figure()
+            plt.hist(labels, bins=n_clusters)
+            plt.xlabel('Label')
+            plt.show()
+            # plt.savefig(f'hist_nc{n_clusters}_idx{idx}')
+            plt.close()
+
+        # remove_label(labels, centers)
+
+        print(stats.describe(count))
+        df_describe = pd.DataFrame(count)
+        print(df_describe.describe())
+
+        # lin_reg.score(lin_reg.predict(X), y)
+        # t =  np.dot(poly_reg.coef_, X_poly[2].reshape(-1,1))
+
+        bp = 0
+
 
     bp = 0
 
