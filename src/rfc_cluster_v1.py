e7f8ae0e03ccf13a7b9e86410d1014cfb665f2f0
Dave <davemers@iu.edu>
2021-09-06 23:09:33 -0400

    working on curve fitting

--- src/rfc_cluster_v1.py
+++ src/rfc_cluster_v1.py
@@ -58,41 +58,41 @@ def sum_sine_3(x, a1, b1, c1, a2, b2, c2, a3, b3, c3):
     return a1*np.sin(b1*x+c1) + a2*np.sin(b2*x+c2) + a3*np.sin(b3*x+c3)
 
 
-def sum_sine_3_bfgs(P, x):
-    res = P[0]*np.sin(P[1]*x+P[2]) + P[3]*np.sin(P[4]*x+P[5]) + P[6]*np.sin(P[7]*x+P[8])
-    return np.sum((res - x)*(res - x))
+# def sum_sine_3_bfgs(P, x):
+#     res = P[0]*np.sin(P[1]*x+P[2]) + P[3]*np.sin(P[4]*x+P[5]) + P[6]*np.sin(P[7]*x+P[8])
+#     return np.sum((res - x)*(res - x))
 
-def sum_sine3_pso(P, x, y):
-    # p = C.shape[0]
+# def sum_sine3_pso(P, x, y):
+#     # p = C.shape[0]
+#
+#     f_loss = np.empty(0)
+#
+#     for idx in range(0, P.shape[0]):
+#         res = P[idx, 0]*np.sin(P[idx, 1]*x+P[idx, 2]) + P[idx, 3]*np.sin(P[idx, 4]*x+P[idx, 5]) + P[idx, 6]*np.sin(P[idx, 7]*x+P[idx, 8])
+#         # f_loss = np.append(f_loss, np.sum((res - Y)*(res - Y)))
+#         f_loss = np.append(f_loss, 1-r_squared(res, y))
+#
+#     return f_loss
 
-    f_loss = np.empty(0)
-
-    for idx in range(0, P.shape[0]):
-        res = P[idx, 0]*np.sin(P[idx, 1]*x+P[idx, 2]) + P[idx, 3]*np.sin(P[idx, 4]*x+P[idx, 5]) + P[idx, 6]*np.sin(P[idx, 7]*x+P[idx, 8])
-        # f_loss = np.append(f_loss, np.sum((res - Y)*(res - Y)))
-        f_loss = np.append(f_loss, 1-r_squared(res, y))
-
-    return f_loss
-
-max_epochs = 30000
+# max_epochs = 30000
 
 # number of random samples to generate (should be a multiple of two for flattening an IQ pair)
-io_size = 32
+io_size = 256
 feature_size = 1
 cluster_size = 8
 read_data = True
 
 # setup everything
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = "cpu"
-model = AE(io_size, feature_size).to(device)
-decoder = Decoder(feature_size, io_size).to(device)
-
-# this is setup as a static learning rate.  we may want to look at variable lr based on some performance numbers
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-criterion = nn.MSELoss()
-
-dec_opt = optim.Adam(decoder.parameters(), lr=1e-3)
+# device = "cpu"
+# model = AE(io_size, feature_size).to(device)
+# decoder = Decoder(feature_size, io_size).to(device)
+#
+# # this is setup as a static learning rate.  we may want to look at variable lr based on some performance numbers
+# optimizer = optim.Adam(model.parameters(), lr=1e-3)
+# criterion = nn.MSELoss()
+#
+# dec_opt = optim.Adam(decoder.parameters(), lr=1e-3)
 
 if __name__ == '__main__':
 
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     # fp_bits = 4
 
     # features for the decoder
-    F_np = 2048*np.ones(feature_size).astype(np.float32)
+    # F_np = 2048*np.ones(feature_size).astype(np.float32)
 
     # F = torch.from_numpy((2048*np.ones((1, feature_size))).astype(np.float32)).to(device)
     # F = F.view(-1, feature_size)
@@ -113,13 +113,13 @@ if __name__ == '__main__':
     print("Loading data...\n")
 
     base_name = "sdr_test"
-    xd = np.fromfile("../data/" + base_name + "_10M_100m_0001.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
+    iq_data = np.fromfile("../data/" + base_name + "_10M_100m_0001.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
 
 
     # base_name = "VH1-164"
     # xd = np.fromfile("e:/data/zsl/" + base_name + ".sigmf-data.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
 
-    x_blocks = math.ceil(xd.size/io_size)
+    x_blocks = math.ceil(iq_data.size/io_size)
     data_type = "sdr"
 
     date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -132,11 +132,11 @@ if __name__ == '__main__':
     # test_writer = open((log_dir + base_name + "_{:02d}-bits_M3_".format(fp_bits) + "data.bin"), "wb")
 
 
-    # cycle through the decoder side and do whatever needs doing
-    for parameter in model.decoder.parameters():
-        parameter.requires_grad = False        # uncomment to freeze the training of these layers
-        p = parameter.data
-        bp = 0
+    # # cycle through the decoder side and do whatever needs doing
+    # for parameter in model.decoder.parameters():
+    #     parameter.requires_grad = False        # uncomment to freeze the training of these layers
+    #     p = parameter.data
+    #     bp = 0
 
 
     print("Processing...\n")
@@ -144,221 +144,45 @@ if __name__ == '__main__':
     idx = 50000
 
     # step 2: the block to process
-    x = xd[idx:(idx + io_size)]
+    y_data = iq_data[idx:(idx + io_size)]
 
     # step 3: this is the initial weights for the network
-    xw = x / F_np
+    # xw = x / F_np
 
     # step 4: cluster the data
-    cluster_results = KMeans(n_clusters=cluster_size).fit(xw.reshape(-1, 1))
-
-    data = xw.reshape(-1, 1)
-    for cluster in range(cluster_size):
-        t1 = np.float32(cluster_results.labels_ == cluster).reshape(-1, 1)
-        t2 = 1-t1
-
-        data = data*t2
-        t1 = t1 * cluster_results.cluster_centers_[cluster]
-        data += t1
-
-    model.decoder.output_layer.weight.data = nn.Parameter(torch.from_numpy(data.reshape(model.decoder.output_layer.weight.shape)))
-
-    bp = 0
-
-
-
-    # for layer in model.decoder.parameters():
-    #     data = layer.detach().numpy()
-    #     data = data.reshape(-1, 1)
+    # cluster_results = KMeans(n_clusters=cluster_size).fit(xw.reshape(-1, 1))
     #
-    #     layer.data = nn.Parameter(torch.from_numpy(data.reshape(layer.shape)))
-    #     bp = 0
-
-
-
-    X = torch.from_numpy(x).to(device)
-    X = X.view(-1, x.size)
-
-
-    # model must be set to train mode
-    # model.train()
+    # data = xw.reshape(-1, 1)
+    # for cluster in range(cluster_size):
+    #     t1 = np.float32(cluster_results.labels_ == cluster).reshape(-1, 1)
+    #     t2 = 1-t1
     #
-    # print("block {:}".format(idx))
-    # for epoch in range(max_epochs):
-    #     loss = 0
-    #     optimizer.zero_grad()
-    #     XO = model(X)
+    #     data = data*t2
+    #     t1 = t1 * cluster_results.cluster_centers_[cluster]
+    #     data += t1
     #
-    #     train_loss = criterion(XO, X)
-    #     loss += train_loss.item()
+    # model.decoder.output_layer.weight.data = nn.Parameter(torch.from_numpy(data.reshape(model.decoder.output_layer.weight.shape)))
     #
-    #     print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, max_epochs, loss))
+
+
+    # X = torch.from_numpy(x).to(device)
+    # X = X.view(-1, x.size)
     #
-    #     loss_q = torch.sum(torch.abs(torch.floor(XO + 0.5) - X))
-    #     if (loss < 0.15):
-    #         bp = 10
-    #         break
     #
-    #     train_loss.backward()
-    #     optimizer.step()
+    # #
+    # # F2 = model.encoder(X)
     #
-    # loss = torch.sum(torch.abs(torch.floor(XO + 0.5) - X))
-    # print("loss = {:.6f}\n".format(loss.item()))
+    # F2 = torch.from_numpy(F_np).to(device)
+    # XO2 = model.decoder(F2)
 
+
+    # dist_mean, dist_std, phase_mean, phase_std = zsl_error_metric(X.numpy(), XO2.detach().numpy())
     #
-    # F2 = model.encoder(X)
-
-    F2 = torch.from_numpy(F_np).to(device)
-    XO2 = model.decoder(F2)
-
-
-    # for idx in range(0, x_blocks*io_size, io_size):
-        # x = xd[idx:(idx + io_size)]
-        #
-        # # get the mean of x
-        # x_mean = math.floor(np.mean(x))
-        # x_std = np.std(x)
-        #
-        # # convert x into a torch tensor variable
-        # X = torch.from_numpy(x).to(device)
-        # X = X.view(-1, x.size)
-        #
-        # if (x.size < io_size):
-        #     X = torch.nn.functional.pad(X, (0, io_size-x.size))
-
-        # create a data logger to save info on the run
-        # data_writer = open((log_dir + scenario_name + "_" + date_time + ".txt"), "w")
-        #
-        # data_writer.write("#-------------------------------------------------------------------------------\n")
-        # data_writer.write("# data bits, min, max:\n{}, {}, {}\n\n".format(data_bits, data_min, data_max))
-        # data_writer.write("# io_size:\n{}\n\n".format(io_size))
-        # data_writer.write("# feature_size:\n{}\n\n".format(feature_size))
-        # data_writer.write("# F:\n")
-
-        # for idx in range(feature_size):
-        #     data_writer.write("{:.6f}".format((F.numpy())[0][idx]))
-        #     if(idx < feature_size-1):
-        #         data_writer.write(", ")
-        #     else:
-        #         data_writer.write("\n\n")
-
-        # # model must be set to train mode
-        # model.train()
-
-        # print("block {:}".format(idx))
-        # for epoch in range(max_epochs):
-        #     loss = 0
-        #     optimizer.zero_grad()
-        #     outputs = model(F)
-        #
-        #     train_loss = criterion(outputs, X)
-        #     loss += train_loss.item()
-        #
-        #     # print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, max_epochs, loss))
-        #
-        #     loss_q = torch.sum(torch.abs(torch.floor(outputs + 0.5) - X))
-        #     if (loss_q < 1):
-        #         bp = 10
-        #         break
-        #
-        #     train_loss.backward()
-        #     optimizer.step()
-        #
-        # loss = torch.sum(torch.abs(torch.floor(outputs + 0.5) - X))
-        # print("loss = {:.6f}\n".format(loss.item()))
-
-        # data_writer.write("#-------------------------------------------------------------------------------\n")
-        # data_writer.write("# final_loss:\n{:.4f}\n\n".format(loss.item()))
-        # data_writer.write("# X:\n")
-        #
-        # for idx in range(io_size):
-        #     data_writer.write("{}".format((X.numpy())[0][idx]))
-        #     if(idx<io_size-1):
-        #         data_writer.write(", ")
-        #     else:
-        #         data_writer.write("\n")
-        #
-        # data_writer.close()
-
-    # -------------------------------------------------------------------------------
-    # training complete
-    # -------------------------------------------------------------------------------
-
-        # model.eval()
-        #
-        # # get the weights
-        # dw1a = copy.deepcopy(model.output_layer.weight.data).t()
-        #
-        # # Set-up hyperparameters
-        # pso_options = {'c1': 2.1, 'c2': 2.0, 'w': 1.1}
-        #
-        # time.sleep(1)
-        #
-        # # -------------------------------------------------------------------------------
-        # # run PSO to find the best scale value
-        # # -------------------------------------------------------------------------------
-        #
-        # # for fp_bits in range(8, 13):
-        #
-        # # the min/max number (0 <= x < fp_range)
-        # # fp_range = 2**(fp_bits)      # the max value
-        # # fp_min = 0
-        # # fp_max = fp_range - 1
-        #
-        # # the min/max number (fp_range <= x < fp_range-1)
-        # fp_range = 2**(fp_bits-1)      # the max value
-        # fp_min = -fp_range
-        # fp_max = fp_range - 1
-        #
-        # scale_step = 0.005
-        #
-        # # min_scale = math.floor(fp_range * 0.25)
-        # # max_scale = (fp_range * 0.625) + scale_step
-        # # min_loss = 1e10
-        # min_scale = (2**(fp_bits-2)) *0.625
-        # max_scale = (fp_range * 1.1) + scale_step
-        #
-        # # scale_bounds = [min_scale, max_scale]
-        # scale_bounds = (min_scale*np.ones(1), max_scale*np.ones(1))
-        #
-        # # data_wr = open((log_dir + scenario_name + "_{:02d}-bits_".format(fp_bits) + date_time + ".txt"), "w")
-        #
-        # pso_opt = ps.single.GlobalBestPSO(n_particles=40, dimensions=1, options=pso_options, bounds=scale_bounds)
-        #
-        # # pso_opt.optimize(fx.sphere, iters=100)
-        # # cost, scale = pso_opt.optimize(get_best_scale, iters=50, F=F.numpy(), W=dw1a.numpy(), X=X.numpy(), fp_min=fp_min, fp_max=fp_max)
-        # scale = [8.0]
-        #
-        # # data_wr.write("{:0.8f}, {}, ".format(scale[0], cost))
-        #
-        # dw1a_q = torch.clamp_min(torch.clamp_max(torch.floor(torch.abs(dw1a * scale[0])), fp_max), fp_min)
-        # d1 = (torch.sign(dw1a)*dw1a_q)/scale[0]
-        #
-        # w_q = quantize_weights(dw1a.numpy(), scale[0])
-        #
-        #
-        # # Y = torch.floor((d1.t()*F).sum(axis=1) + 0.5)
-        # Y = torch.floor(torch.sum(torch.from_numpy(w_q).t()*F, dim=1) + 0.5)
-        # loss2 = torch.sum(torch.abs(Y - X))/X.shape[1]
-        #
-        # print("scale = {:0.6f}, loss = {:.2f}".format(scale[0], loss2.item()))
-
-        # for idx in range(io_size):
-        #     data_wr.write("{}".format((Y.numpy())[idx]))
-        #     if(idx<io_size-1):
-        #         data_wr.write(", ")
-        #     else:
-        #         data_wr.write("\n")
-        #
-        # data_wr.close()
-
-    dist_mean, dist_std, phase_mean, phase_std = zsl_error_metric(X.numpy(), XO2.detach().numpy())
-
-    print("dist_mean = {:0.4f}, dist_std = {:0.4f}, phase_mean = {:0.4f}, phase_std = {:0.4f}".format(dist_mean, dist_std, phase_mean, phase_std))
+    # print("dist_mean = {:0.4f}, dist_std = {:0.4f}, phase_mean = {:0.4f}, phase_std = {:0.4f}".format(dist_mean, dist_std, phase_mean, phase_std))
 
     # try to do curve fitting
-    c_x = np.arange(0, io_size, 1)
-    c_y = data.reshape(-1)
+    x_data = np.arange(0, io_size, 1)
+    y_data = y_data.reshape(-1)
 
     # numpy fitting
 
@@ -370,7 +194,7 @@ if __name__ == '__main__':
     # cost, P = pso_opt.optimize(sum_sine3_pso, iters=200, x=c_x, y=xw)
     # print(P)
     #
-    # c_x2 = np.arange(0, io_size, 0.01)
+    x_data_hr = np.arange(0, io_size, 0.01)
     # plt.scatter(c_x, xw, c='blue', s=1, label='data')
     # # plt.plot(c_x, func(c_x, *popt), 'r-', label='fit')
     # # plt.plot(c_x, poly4(c_x, *popt), 'r-', label='fit')
@@ -384,52 +208,52 @@ if __name__ == '__main__':
     # scipy fitting
     # popt, pcov = curve_fit(func, c_x, c_y)
     # popt, pcov = curve_fit(poly4, c_x, c_y)
-    popt, pcov = curve_fit(sum_sine_3, c_x, c_y)
+    popt, pcov = curve_fit(sum_sine_3, x_data, y_data)
 
     print(popt)
 
     # plt.plot(c_x, c_y, 'b-', label='data')
-    plt.scatter(c_x, c_y, c='blue', s=1, label='data')
+    plt.scatter(x_data, y_data, c='blue', s=1, label='data')
     # plt.plot(c_x, func(c_x, *popt), 'r-', label='fit')
     # plt.plot(c_x, poly4(c_x, *popt), 'r-', label='fit')
-    plt.plot(c_x2, sum_sine_3(c_x2, *popt), 'r-', label='fit')
+    plt.plot(x_data_hr, sum_sine_3(x_data_hr, *popt), 'r-', label='fit')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
     plt.show()
 
 
-    # popt2, pcov2 = curve_fit(func, c_x, xw)
-    # popt2, pcov2 = curve_fit(func, c_x, xw)
-    popt2, pcov2 = curve_fit(sum_sine_3, c_x, xw)
-    print(popt2)
-
-    plt.scatter(c_x, xw, c='blue', s=1, label='data')
-    # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    plt.plot(c_x2, sum_sine_3(c_x2, *popt2), 'r-', label='fit:')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.show()
+    # # popt2, pcov2 = curve_fit(func, c_x, xw)
+    # # popt2, pcov2 = curve_fit(func, c_x, xw)
+    # popt2, pcov2 = curve_fit(sum_sine_3, c_x, xw)
+    # print(popt2)
+    #
+    # plt.scatter(c_x, xw, c='blue', s=1, label='data')
+    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
+    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
+    # plt.plot(c_x2, sum_sine_3(c_x2, *popt2), 'r-', label='fit:')
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.legend()
+    # plt.show()
 
 
     # bfgs optimization
-    x0 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
+    # x0 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
 
 
 
-    xopt = fmin_bfgs(sum_sine_3_bfgs, x0, args=(c_x,))
-    print(xopt)
-
-    plt.scatter(c_x, xw, c='blue', s=1, label='data')
-    # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    plt.plot(c_x2, sum_sine_3_bfgs(xopt, c_x2), 'r-', label='fit:')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.show()
+    # xopt = fmin_bfgs(sum_sine_3_bfgs, x0, args=(c_x,))
+    # print(xopt)
+    #
+    # plt.scatter(c_x, xw, c='blue', s=1, label='data')
+    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
+    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
+    # plt.plot(c_x2, sum_sine_3_bfgs(xopt, c_x2), 'r-', label='fit:')
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.legend()
+    # plt.show()
 
 
     # # write the reconstructed data to a binary file
--- src/rfc_curve_fit_v1.py
+++ src/rfc_curve_fit_v1.py
@@ -58,42 +58,16 @@ def poly4(x, c0, c1, c2, c3, c4):
 def sum_sine_3(x, a1, b1, c1, a2, b2, c2, a3, b3, c3):
     return a1*np.sin(b1*x+c1) + a2*np.sin(b2*x+c2) + a3*np.sin(b3*x+c3)
 
-
-def sum_sine_3_bfgs(P, x):
-    res = P[0]*np.sin(P[1]*x+P[2]) + P[3]*np.sin(P[4]*x+P[5]) + P[6]*np.sin(P[7]*x+P[8])
-    return np.sum((res - x)*(res - x))
-
-def sum_sine3_pso(P, x, y):
-    # p = C.shape[0]
-
-    f_loss = np.empty(0)
-
-    for idx in range(0, P.shape[0]):
-        res = P[idx, 0]*np.sin(P[idx, 1]*x+P[idx, 2]) + P[idx, 3]*np.sin(P[idx, 4]*x+P[idx, 5]) + P[idx, 6]*np.sin(P[idx, 7]*x+P[idx, 8])
-        # f_loss = np.append(f_loss, np.sum((res - Y)*(res - Y)))
-        f_loss = np.append(f_loss, 1-r_squared(res, y))
-
-    return f_loss
+def sum_sine_4(x, a1, b1, c1, a2, b2, c2, a3, b3, c3, a4, b4, c4):
+    return a1*np.sin(b1*x+c1) + a2*np.sin(b2*x+c2) + a3*np.sin(b3*x+c3) + a4*np.sin(b4*x+c4)
 
 max_epochs = 30000
 
 # number of random samples to generate (should be a multiple of two for flattening an IQ pair)
-io_size = 32
+io_size = 128
 feature_size = 1
 cluster_size = 8
 read_data = True
-
-# setup everything
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = "cpu"
-model = AE(io_size, feature_size).to(device)
-decoder = Decoder(feature_size, io_size).to(device)
-
-# this is setup as a static learning rate.  we may want to look at variable lr based on some performance numbers
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-criterion = nn.MSELoss()
-
-dec_opt = optim.Adam(decoder.parameters(), lr=1e-3)
 
 if __name__ == '__main__':
 
@@ -104,115 +78,91 @@ if __name__ == '__main__':
     # data_max = 2**data_bits
     # fp_bits = 4
 
-    # features for the decoder
-    F_np = 2048*np.ones(feature_size).astype(np.float32)
-
-    # F = torch.from_numpy((2048*np.ones((1, feature_size))).astype(np.float32)).to(device)
-    # F = F.view(-1, feature_size)
-
     # step 1: load the data
     print("Loading data...\n")
 
     base_name = "sdr_test"
-    yd = np.fromfile("../data/" + base_name + "_10M_100m_0001.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
+    iq_data = np.fromfile("../data/" + base_name + "_10M_100m_0001.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
 
 
     # base_name = "VH1-164"
     # xd = np.fromfile("e:/data/zsl/" + base_name + ".sigmf-data.bin", dtype=np.int16, count=-1, sep='', offset=0).astype(np.float32)
 
-    y_blocks = math.ceil(yd.size/io_size)
+    y_blocks = math.ceil(iq_data.size/io_size)
     data_type = "sdr"
 
     date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    scenario_name = "fs{:02d}-io{:03d}-".format(feature_size, io_size) + data_type
+    scenario_name = "sin3_io{:03d}_".format(io_size) + data_type
     log_dir = "../results/" + scenario_name + "/"
 
     os.makedirs(log_dir, exist_ok=True)
 
     # writer to save the data
-    # test_writer = open((log_dir + base_name + "_{:02d}-bits_M3_".format(fp_bits) + "data.bin"), "wb")
+    test_writer = open((log_dir + base_name + "_reconstructed_data.bin"), "wb")
 
     print("Processing...\n")
 
     idx = 50000
+    for idx in range(0, y_blocks*io_size, io_size):
 
-    # step 2: the block to process
-    c_y = yd[idx:(idx + io_size)].reshape(-1)
-    c_x = np.arange(0, io_size, 1)
-    c_x2 = np.arange(0, io_size, 0.01)
+        # step 1: the block to process
+        y_data = iq_data[idx:(idx + io_size)].reshape(-1)
+        x_data = np.arange(0, y_data.shape[0], 1)
+        x_data_hr = np.arange(0, y_data.shape[0], 0.01)
 
-    sine_size = 3
-    # create initila guess based on frequency content
-    x0 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
-    y0 = np.ones([sine_size*3], dtype=np.float32)
+        sine_size = 4
 
-    fy = np.fft.fft(c_y)
-    for idx in range(0, sine_size, 1):
-        y0[3*idx] = np.mean(np.abs(c_y))/(idx+1)
+        # create initial guess based on frequency content
+        x0 = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
+        y0 = np.ones([sine_size*3], dtype=np.float32)
 
-        ml = np.argmax(np.abs(fy[0:math.floor(io_size/2)]))
-        y0[3*idx+1] = 2 * np.pi * (max(0.5, ml)) / (c_x[-1] - c_x[0])
-        fy[ml] = 0
+        fy = np.fft.fft(y_data)
+        for jdx in range(0, sine_size, 1):
+            y0[3*jdx] = np.mean(np.abs(y_data))/(jdx+1)
 
-        bp = 0
+            ml = np.argmax(np.abs(fy[0:math.floor(io_size/2)]))
+            y0[3*jdx+1] = 2 * np.pi * (max(0.5, ml)) / (x_data[-1] - x_data[0])
+            fy[ml] = 0
 
-    # scipy fitting
-    # popt, pcov = curve_fit(func, c_x, c_y)
-    # popt, pcov = curve_fit(poly4, c_x, c_y)
-    popt, pcov = curve_fit(f=sum_sine_3, xdata=c_x, ydata=c_y, p0=y0)
+            bp = 0
 
-    print(popt)
-
-    # plt.plot(c_x, c_y, 'b-', label='data')
-    plt.scatter(c_x, c_y, c='blue', s=1, label='data')
-    plt.plot(c_x2, sum_sine_3(c_x2, *popt), 'r-', label='fit')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.legend()
-    plt.show()
-
-    # get the reconstructed value
-    y_hat = sum_sine_3(c_x, *popt)
+        try:
+            # scipy fitting
+            popt, pcov = curve_fit(f=sum_sine_4, xdata=x_data, ydata=y_data, p0=y0)
 
 
+            # plt.scatter(x_data, y_data, c='blue', s=1, label='data')
+            # plt.plot(x_data_hr, sum_sine_3(x_data_hr, *popt), 'r-', label='fit')
+            # plt.xlabel('x')
+            # plt.ylabel('y')
+            # plt.legend()
+            # plt.show()
 
-    # # popt2, pcov2 = curve_fit(func, c_x, xw)
-    # # popt2, pcov2 = curve_fit(func, c_x, xw)
-    # popt2, pcov2 = curve_fit(sum_sine_3, c_x, xw)
-    # print(popt2)
-    #
-    # plt.scatter(c_x, xw, c='blue', s=1, label='data')
-    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # plt.plot(c_x2, sum_sine_3(c_x2, *popt2), 'r-', label='fit:')
-    # plt.xlabel('x')
-    # plt.ylabel('y')
-    # plt.legend()
-    # plt.show()
-    #
-    #
-    # # bfgs optimization
-    #
-    #
-    #
-    #
-    # xopt = fmin_bfgs(sum_sine_3_bfgs, x0, args=(c_x,))
-    # print(xopt)
-    #
-    # plt.scatter(c_x, xw, c='blue', s=1, label='data')
-    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # # plt.plot(c_x, func(c_x, *popt2), 'r-', label='fit:')
-    # plt.plot(c_x2, sum_sine_3_bfgs(xopt, c_x2), 'r-', label='fit:')
-    # plt.xlabel('x')
-    # plt.ylabel('y')
-    # plt.legend()
-    # plt.show()
+            # get the reconstructed value
+            y_hat = np.floor(sum_sine_4(x_data, *popt) + 0.5)
 
-    # calculate the metrics
-    dist_mean, dist_std, phase_mean, phase_std = zsl_error_metric(c_y, y_hat)
-    print("dist_mean = {:0.4f}, dist_abs = {:0.4f}, phase_mean = {:0.4f}, phase_std = {:0.4f}".format(dist_mean, dist_std, phase_mean, phase_std))
+            # print(popt)
+            #
+        except RuntimeError:
+            print("RuntimeError")
 
-    print("r_squared = {:0.4f}\n".format(r2_score(c_y, y_hat)))
+            y_hat = y_data
+
+        # calculate the metrics
+        dist_mean, dist_std, phase_mean, phase_std = zsl_error_metric(y_data, y_hat)
+        print("block {:}: dist_mean = {:0.4f}, dist_abs = {:0.4f}, phase_mean = {:0.4f}, phase_std = {:0.4f}".format(idx, dist_mean, dist_std, phase_mean, phase_std))
+
+        print("r_squared = {:0.4f}\n".format(r2_score(y_data, y_hat)))
+
+        bp = 1
+
+        # write the reconstructed data to a binary file
+        # # t2 = (Y.numpy())[0:x.size]
+        # # t2 = y_hat.astype(np.int16)
+        test_writer.write(y_hat.astype(np.int16))
+
+    test_writer.close()
+
 
     # just a stopping break point before the code ends
     bp = 9
